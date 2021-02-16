ed6838dc674233e235264a2e9bc58e467d07a238
Ganesh-11062000 <ganeshnm2000@gmail.com>
2021-02-16 16:11:53 +0530

    implementation in java

---
+++ RemoteInterfaceImplementation.java
@@ -0,0 +1,8 @@
+// Implementing the remote interface 
+public class RemoteInterfaceImplementation implements RemoteInterface{  
+   
+   // Implementing the interface method 
+   public void printHelloWorld() {  
+      System.out.println("Hello World printed by Server");  
+   }  
+} 
